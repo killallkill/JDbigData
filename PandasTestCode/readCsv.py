e099e17b239d153fdec94326252300690cb0d9a4
killallkill <990691833@qq.com>
2017-04-11 17:12:16 +0800

    time test

--- PandasTestCode/readCsv.py
+++ PandasTestCode/readCsv.py
@@ -1,6 +1,8 @@
 # -*- coding=utf-8 -*-
-
+import datetime, time
 import pandas as pd
+import re
+
 
 class GetCSV():
 
@@ -12,9 +14,28 @@ class GetCSV():
                 print "col name %s  col value %d" % (col_name, row[col_name])
             print "-----------end row-----------"
 
+    def getUser(self):
+        uData = pd.read_csv(filepath_or_buffer="F:\jdata\JData\JData_User.csv",encoding="gbk")
+        for index, row in uData.iterrows():
+            print "--------row %d--------" % index
+            dt = row["user_reg_tm"]
+            t = time.strptime(dt, "%Y-%m-%d")
+            y, m, d = t[:3]
+            dt1 = datetime.date(y, m, d).strftime("%Y%m%d")
+            print dt1
+            for col in uData.columns:
+                '''
+                if col.__eq__("age"):
+                    numPatten = re.compile("\d{0,2}-\d{0,2} | \d{0,2}")
+                    reResult = re.findall(numPatten, row[col].encode("utf-8"))
+                    print reResult
+                '''
 
+
+
+            print "------end row %d------" % index
 
 if __name__ == "__main__":
     csvObj = GetCSV()
-    csvObj.readCSV()
+    csvObj.getUser()
 
